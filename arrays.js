e92abd121252c3d83d9d444098c2f633630fe5c8
Brian <briandsalemi@gmail.com>
2018-09-18 15:20:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -45,5 +45,5 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 
 function removeElementFromEndOfArray(array) {
   array.slice(1, array.length - 1);
-  return array;
+  return array();
 }
